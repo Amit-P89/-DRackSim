c7c06a47c7cf89799c6b090f2e1767feef3fe435
Amit-P89 <92443674+Amit-P89@users.noreply.github.com>
2023-07-17 19:01:23 +0530

    Update Mem_Trace.cpp

--- DRackSim-Trace/Trace_Tool/Mem_Trace.cpp
+++ DRackSim-Trace/Trace_Tool/Mem_Trace.cpp
@@ -1,19 +1,3 @@
-/*
- * Copyright 2002-2019 Intel Corporation.
- * 
- * This software is provided to you as Sample Source Code as defined in the accompanying
- * End User License Agreement for the Intel(R) Software Development Products ("Agreement")
- * section 1.L.
- * 
- * This software and the related documents are provided as is, with no express or implied
- * warranties, other than those that are expressly stated in the License.
- */
-
-/*! @file
- *  This file contains an ISA-portable PIN tool for functional simulation of
- *  instruction+data TLB+cache hierarchies
- */
-
 using namespace std;
 #define CORE_NUM 8
 #define TLB_PENALTY 10
